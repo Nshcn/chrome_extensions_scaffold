7be4a085eb5dee576f6fc94124d6c3fc0b2684b0
zhanwenqing <2695563464@qq.com>
2025-02-09 13:44:21 +0800

    feat: 修正构建逻辑

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
     react(),
     crx({ manifest }),
   ],
+  build: {
+    // 防止 CSS 代码分割
+    cssCodeSplit: false,
+    // 防止代码分割
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 }); 
